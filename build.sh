ebfbc63a206f34eb849219d107ca0b2a67b419a4
Kononenko Oleg <o.kononenko@qiwi.com>
2025-03-10 13:52:49 +0300

    Fix build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 # Copyright, Aleksey Konovkin (alkon2000@mail.ru)
 # BSD license type
@@ -46,8 +46,7 @@ function build_debug() {
               --with-stream \
               --with-debug \
               --with-cc-opt="-O0" \
-              --add-module=../../../ngx_dynamic_healthcheck \
-              --add-module=../ngx_dynamic_upstream > /dev/null
+              --add-module=../
 
   r=$?
   if [ $r -ne 0 ]; then
@@ -75,102 +74,7 @@ function build_release() {
               $EMBEDDED_OPTS \
               --with-stream \
               --add-module=../../../ngx_dynamic_healthcheck \
-              --add-module=../ngx_dynamic_upstream > /dev/null
-
-  r=$?
-  if [ $r -ne 0 ]; then
-    exit $r
-  fi
-
-  echo "Build release nginx-$VERSION$SUFFIX"
-  make -j4 > /dev/null
-
-  r=$?
-  if [ $r -ne 0 ]; then
-    exit $r
-  fi
-  make install > /dev/null
-  cd ..
-}
-
-function download_module() {
-  if [ -e $DIR/../$2 ]; then
-    echo "Get $DIR/../$2"
-    dir=$(pwd)
-    cd $DIR/..
-    tar zcf $dir/$2.tar.gz $(ls -1d $2/* | grep -vE "(install$)|(build$)|(download$)|(.git$)")
-    cd $dir
-  else
-    if [ $download -eq 1 ] || [ ! -e $2.tar.gz ]; then
-      echo "Download $2 branch=$3"
-      curl -s -L -o $2.zip https://github.com/$1/$2/archive/$3.zip
-      unzip -q $2.zip
-      mv $2-* $2
-      tar zcf $2.tar.gz $2
-      rm -rf $2-* $2 $2.zip
-    fi
-  fi
-}
-
-function gitclone() {
-  git clone $1 > /dev/null 2> /tmp/err
-  if [ $? -ne 0 ]; then
-    cat /tmp/err
-  fi
-}
-
-function download_nginx() {
-  if [ $download -eq 1 ] || [ ! -e nginx-$VERSION.tar.gz ]; then
-    echo "Download nginx-$VERSION"
-    curl -s -L -O http://nginx.org/download/nginx-$VERSION.tar.gz
-  else
-    echo "Get nginx-$VERSION.tar.gz"
-  fi
-}
-
-function download_pcre() {
-  if [ $download -eq 1 ] || [ ! -e pcre2-$PCRE2_VERSION.tar.gz ]; then
-    echo "Download PCRE2-$PCRE2_VERSION"
-    curl -s -L -O http://ftp.cs.stanford.edu/pub/exim/pcre/pcre2-$PCRE2_VERSION.tar.gz
-  else
-    echo "Get pcre2-$PCRE2_VERSION.tar.gz"
-  fi
-}
-
-function download_dep() {
-  if [ $download -eq 1 ] || [ ! -e $2-$3.tar.gz ]; then
-    echo "Download $2-$3.$4"
-    curl -s -L -o $2-$3.tar.gz $1/$2-$3.$4
-  else
-    echo "Get $2-$3.tar.gz"
-  fi
-}
-
-function extract_downloads() {
-  cd download
-
-  for d in $(ls -1 *.tar.gz)
-  do
-    echo "Extracting $d"
-    tar zxf $d -C ../build --keep-old-files 2>/dev/null
-  done
-
-  cd ..
-}
-
-function download() {
-  mkdir build                2>/dev/null
-  mkdir build/deps           2>/dev/null
-
-  mkdir download             2>/dev/null
-  mkdir download/lua_modules 2>/dev/null
-
-  cd download
-
-  download_pcre
-  download_nginx
-
-  download_module ZigzagAK    ngx_dynamic_upstream             master
+              --add-module=../
 
   download_dep http://zlib.net                                 zlib      $ZLIB_VERSION      tar.gz
 
