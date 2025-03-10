2c293621d04c6fb82a67fa0e3400560e74a02f3e
Kononenko Oleg <o.kononenko@qiwi.com>
2025-03-10 14:26:33 +0300

    Fix build script

--- build.sh
+++ build.sh
@@ -10,7 +10,14 @@ mkdir -p build/
 wget "https://nginx.org/download/nginx-${NGINX_VERSION}.tar.gz"
 ls -la
 
-tar -xvzf nginx-${NGINX_VERSION}.tar.gz -C build/
+tar -xzf nginx-${NGINX_VERSION}.tar.gz
+mv nginx-${NGINX_VERSION}/* build/
+mkdir build/modules/ngx_healthcheck/
+mv src  build/modules/ngx_healthcheck/
+mv config  build/modules/ngx_healthcheck/
 cd build/
 ls -la ./
 ls -la ./modules/
+ls -la ./modules/ngx_healthcheck/
+./conifure --with-pcre --with-http_ssl_module --add-module=./modules/ngx_healthcheck/
+make -j8
